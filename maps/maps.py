7ec4a798753e0366bfea6af6b707f132c8b5ca5c
Artem Basalaev <basa62.1996@gmail.com>
2023-09-03 01:09:57 +0200

    Implement maps

--- maps/maps.py
+++ maps/maps.py
@@ -13,7 +13,22 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+        filtered_movies = filter(
+            lambda m: MapExercise.get_movie_rating_kinopoisk(m) > 0
+            and len(MapExercise.get_movie_countries(m)) >= 2,
+            list_of_movies,
+        )
+        rating_movies = list(map(MapExercise.get_movie_rating_kinopoisk, filtered_movies))
+        return sum(rating_movies) / len(rating_movies)
+
+    @staticmethod
+    def get_movie_countries(movie: dict) -> list[str]:
+        countries_str = movie.get("country", "")
+
+        countries = countries_str.split(",")
+        countries = map(str.strip, countries)
+
+        return list(countries)
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -28,4 +43,15 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
-        pass
+        filtered_movies = filter(
+            lambda m: MapExercise.get_movie_rating_kinopoisk(m) > rating, list_of_movies
+        )
+        sum_movies = map(lambda m: m["name"].count("и"), filtered_movies)
+        return sum(sum_movies)
+
+    @staticmethod
+    def get_movie_rating_kinopoisk(movie: dict) -> Union[float, int]:
+        rating_str = movie.get("rating_kinopoisk", "")
+        if rating_str in ["", "0"]:
+            return 0
+        return float(rating_str)
